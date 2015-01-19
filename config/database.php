f6b7f2c5d3fea5dfff1532cea165f1268a8e178a
yusung910 <yusung910@nate.com>
2015-01-19 06:42:42 +0000

    DB info remove

--- config/database.php
+++ config/database.php
@@ -54,9 +54,9 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', ''),
-			'database'  => env('DB_DATABASE', ''),
-			'username'  => env('DB_USERNAME', ''),
+			'host'      => env('DB_HOST', 'localhost'),
+			'database'  => env('DB_DATABASE', 'forge'),
+			'username'  => env('DB_USERNAME', 'forge'),
 			'password'  => env('DB_PASSWORD', ''),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
